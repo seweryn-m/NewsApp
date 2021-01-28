7148bae6f4be46f2d4c18f709471b6997cca14f2
Seweryn Małecki <seweryn.m@gmail.com>
2021-01-28 18:09:16 +0100

    Create project

---
+++ NewsApp/NewsApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using NewsApp.ViewModels;
+
+namespace NewsApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
